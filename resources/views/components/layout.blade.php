0f1e7dc32c4359255d8f3f22e4056a9ca21d1b72
Ras Truth <rasrevelation@gmail.com>
2026-01-12 14:12:39 +0000

    dropdown corrected and righted.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,74 +1,41 @@
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top ">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#"><img class="rounded-circle" src="{{ 'images/sdanewlogo.jfif' }}" Style="width: 3rem"></a>
-      <a class="navbar-brand" href="#">SDA Nsuta</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        <ul class="navbar-nav">
-          <li class="nav-item ">
-            <a class="nav-link" href="/">Home</a>
-          </li>
-            <li>
-               <a href="monday" class="nav-link">Monday</a>
-            </li>
-             <li>
-               <a href="teusday" class="nav-link">Teusday</a>
-            </li>
-            <li>
-               <a href="wednesday" class="nav-link">Wednesday</a>
-            </li>
-             <li>
-               <a href="thursday" class="nav-link">Thursday</a>
-            </li>
-            <li>
-               <a href="friday" class="nav-link">Friday</a>
-            </li>
-            <li>
-               <a href="saturday" class="nav-link">Sabbath</a>
-            </li>
-             <li>
-               <a href="sunday" class="nav-link">Sunday</a>
-            </li>
-          <a href="#" class="nav-link dropdown-toggle">Days Activities</a>
-          <ul class="dropdown-menu">
-            <li>
-               <a href="monday" class="dropdown-item">Monday</a>
-            </li>
-             <li>
-               <a href="teusday" class="dropdown-item">Teusday</a>
-            </li>
-             <li>
-               <a href="wednesday" class="dropdown-item">Wednesday</a>
-            </li>
-             <li>
-               <a href="thursday" class="dropdown-item">Thursday</a>
-            </li>
-            <li>
-               <a href="friday" class="dropdown-item">Friday</a>
-            </li>
-            <li>
-               <a href="saturday" class="dropdown-item">Sabbath</a>
-            </li>
-            <li>
-               <a href="sunday" class="dropdown-item">Sunday</a>
-            </li>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img class="rounded-circle" src="{{ asset('images/sdanewlogo.jfif') }}" style="width: 3rem">
+    </a>
+    <a class="navbar-brand" href="#">SDA Nsuta</a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+
+      <!-- LEFT SIDE -->
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/">Home</a>
+        </li>
+      </ul>
+
+      <!-- RIGHT SIDE -->
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+            Days
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end">
+            <li><a class="dropdown-item" href="monday">Monday</a></li>
+            <li><a class="dropdown-item" href="tuesday">Tuesday</a></li>
+            <li><a class="dropdown-item" href="wednesday">Wednesday</a></li>
+            <li><a class="dropdown-item" href="thursday">Thursday</a></li>
+            <li><a class="dropdown-item" href="friday">Friday</a></li>
+            <li><a class="dropdown-item" href="saturday">Sabbath</a></li>
+            <li><a class="dropdown-item" href="sunday">Sunday</a></li>
           </ul>
-          <!--
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Activities</a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="monday">Monday</a></li>
-              <li><a class="dropdown-item" href="teusday">Teusday</a></li>
-              <li><a class="dropdown-item" href="wednesday">Wednesday</a></li>
-              <li><a class="dropdown-item" href="thursday">Thursday</a></li>
-              <li><a class="dropdown-item" href="friday">Friday</a></li>
-              <li><a class="dropdown-item" href="saturday">Sabath</a></li>
-              <li><a class="dropdown-item" href="sunday">Sunday</a></li>
-            </ul>
-          </li>-->
-        </ul>
-      </div>
+        </li>
+      </ul>
+
     </div>
-  </nav>
+  </div>
+</nav>
