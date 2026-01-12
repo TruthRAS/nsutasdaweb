12b14f64ac611c7a8edc3d995be024a36ecf4ef3
Ras Truth <rasrevelation@gmail.com>
2026-01-12 03:32:22 +0000

    first change to wecome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,44 +8,6 @@
 
         <x-aboutus></x-aboutus>
 
-        <section class="py-5">
-            <div class="container-fluid justify-content-center">
-                <div class="leaders">
-                    <h4 class="text-center">Our Leadership</h4>
-                    <hr class="ruler">
-
-                </div>
-
-                <div class="d-flex flex-row">
-
-                    <x-our_leaders>
-                        <x-slot:title>
-                           {{"Hi"}}
-                        </x-slot:title>
-                        <x-slot:name>
-                           {{"Hiiii"}}
-                        </x-slot:name>
-                        <x-slot:email>
-                           {{"Hiii"}}
-                        </x-slot:email>
-                        <x-slot:mobile>
-                           {{"0203201440"}}
-                        </x-slot:mobile>
-                        <x-slot:facebook>
-                           {{"Facebook"}}
-                        </x-slot:facebook>
-                    </x-our_leaders>
-
-
-
-
-
-
-                      </div>
-                </div>
-
-
-        </section>
 
 
 
